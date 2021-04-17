e7a7d7907858e014e88c953ca97efa971a324f4d
yeonnseok <ljinsk3@gmail.com>
2021-04-17 23:13:44 +0900

    front-controller v4

---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -0,0 +1,24 @@
+package hello.servlet.web.frontcontroller.v4.controller;
+
+import hello.servlet.basic.domain.member.Member;
+import hello.servlet.basic.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberSaveControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(final Map<String, String> paramMap, final Map<String, Object> model) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.put("member", member);
+        return "save-result";
+    }
+}
